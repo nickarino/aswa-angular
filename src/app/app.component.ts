67410f6013f577bf887aff9abe20ab22f017ce49
Joe Skeen <joeskeen@outlook.com>
2023-01-30 16:57:49 -0700

    Add Jest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: [ 'app.component.scss' ]
 })
-export class AppComponent {
-  value = 'World';
+export class AppComponent implements OnInit {
+  value = 'Everyone';
+
+  ngOnInit() {
+    this.value = 'World';
+  }
 }
