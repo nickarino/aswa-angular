d72841ef65b4359a80f654f938a46abdf764f834
Joe Skeen <joeskeen@outlook.com>
2023-01-30 16:57:49 -0700

    Add Jest

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,17 @@
+import { AppComponent } from "./app.component";
+
+describe('AppComponent', () => {
+  let subject: AppComponent;
+  
+  beforeEach(() => {
+    subject = new AppComponent();
+  });
+
+  describe('upon initialization', () => {
+    const action = () => subject.ngOnInit();
+
+    beforeEach(action);
+
+    it('should set value to "World"', () => expect(subject.value).toBe('World'));
+  });
+});
